ae53595e1756ff390e89eb726be63d461c011cfd
Kaltxi <e.yastremskiy@gmail.com>
2023-03-13 23:08:25 +0200

    Unsync ios from stdio

--- src/main.cpp
+++ src/main.cpp
@@ -4,6 +4,9 @@
 using namespace quadratics;
 
 int main(const int count, char* args[]) {
+  // Program does heavy std::cout output, C stdio is not used
+  std::ios_base::sync_with_stdio(false);
+
   if (count <= 1) {
     std::cout << "No coefficients were supplied.\n"
               << "Try: `quadratics 1 2 -3`" << std::endl;
